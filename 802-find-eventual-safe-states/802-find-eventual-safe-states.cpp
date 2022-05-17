b64063b06270cc26720bd1c4dac97f8f48ca5c9a
SahilRout <85143180+SahilRout@users.noreply.github.com>
2022-05-17 17:46:06 +0530

    Time: 197 ms (61.90%), Space: 47.3 MB (53.68%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& graph, vector<int> &color, int node)
+    bool cycleDfs(int node,int parent,vector<vector<int>>& graph,vector<int> &vis)
     {
-        if(color[node] == 1) return false;   // If a node is being visited & we visit again (i.e. cycle is detected), return false
-        
-        color[node] = 1;
-        for(int neighbor: graph[node])
+       vis[node]=1;
+        for(auto it : graph[node])
         {
-            if(color[neighbor] == 2) continue;
-            if(color[neighbor] == 1 or !dfs(graph, color, neighbor)) return false;  // If a neighbor is already visited or dfs of the neighbor return false (i.e. cycle is detected), we return false;
+                if(vis[it]==0&&cycleDfs(it,node,graph,vis)||vis[it]==1) return true;
         }
-        
-        color[node] = 2;   // Otherwise, mark the node as safe & return true;
-        return true;
+        vis[node]=2;
+        return false;
     }
-    
-    vector<int> eventualSafeNodes(vector<vector<int>> &graph) 
-    {
-        vector<int> color(graph.size(), 0), result;
-		
-		for(int i=0; i<graph.size(); i++)
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<int> vis(n,0);
+        vector<int> ans;
+        for(int i=0;i<n;i++)
         {
-            if(color[i] == 2 or dfs(graph, color, i)) // Add only if node is safe or dfs return true. DFS returns true, only when there is no cycle
-                result.push_back(i);
+            if(vis[i]==2||!cycleDfs(i,-1,graph,vis))
+            {
+                ans.push_back(i);
+            }
         }
-                
-        return result;
+        
+        return ans;
     }
 };
