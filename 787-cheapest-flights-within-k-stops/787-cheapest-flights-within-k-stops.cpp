cf90fae1d24a80644d4ddb87112f92ed188c97a8
SahilRout <85143180+SahilRout@users.noreply.github.com>
2022-05-27 23:36:34 +0530

    Time: 37 ms (69.29%), Space: 13.6 MB (53.14%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
-          // create adjacency list
-        unordered_map<int,vector<pair<int, int>>> adjList;
-        for( auto f : flights )
-            adjList[f[0]].push_back( { f[1], f[2] } );
-        
-        // minHeap based on cost of distance from source
-        priority_queue< vector<int>, vector<vector<int>>, greater<vector<int>> > minHeap;
-        minHeap.push( { 0, src, 0 } ); // cost, vertex, hops
-        
-        vector<int> dist(n+1, INT_MAX); //to avoid TLE
-        
-        while( !minHeap.empty() ) {
-            auto t = minHeap.top(); minHeap.pop();
-            int cost = t[0];
-            int curr = t[1];
-            int stop = t[2];
-            
-            if( curr == dst )
-                return cost;
-            
-                //Optimization to avoid TLE
-            if(dist[curr]<stop) continue;
-                 dist[curr]=stop;
-
-            if(stop >K ) continue;
-            
-                for( auto next : adjList[curr] )
-                    minHeap.push( { cost+next.second, next.first, stop+1 });
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+         vector<vector<pair<int,int>>>adj(n);
+        vector<int>cost(n,INT_MAX);
+        vector<int>stop(n,INT_MAX);
+        for(auto x:flights){
+            adj[x[0]].push_back({x[1],x[2]});//attach nodes with their adjacent node
+        }
+        cost[src]=0;
+        stop[src]=0;
+        priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<tuple<int,int,int>>>pq;// make min heap to store triplets and always show minimus on the top
+        pq.push({0,src,0});//pq[0]->cost,pq[1]->curr node,pq[2]->number of stops
+        while(!pq.empty()){
+            auto[costs,currcity,stops]=pq.top();
+            pq.pop();
+            if(currcity==dst){// if we reach the destination
+                return costs;
+            }
+            if(stops>k){//if we take stops more than k
+                continue;//not gonna check further with this curr city
+            }
+            for(auto neighbors:adj[currcity]){//check for their adjacent node
+                auto [node,price]=neighbors;
+                if(costs+price<cost[node]||stops<stop[node]){//
+                    pq.push({costs+price,node,stops+1});
+                    cost[node]=costs+price;
+                    stop[node]=stops;
+                }
+                
+            }
         }
         return -1;
     }
