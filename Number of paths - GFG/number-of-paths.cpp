769ecb3af60671d32ab2d3574163c23ad0e3c7ce
SahilRout <85143180+SahilRout@users.noreply.github.com>
2022-06-02 10:28:16 +0530

    Added solution - LeetHub

---
+++ Number of paths - GFG/number-of-paths.cpp
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+ 
+
+ // } Driver Code Ends
+long long  numberOfPaths(int m, int n)
+{
+     int dp[m+1][n+1];
+        for(int i=0;i<=m;i++)
+        {
+            for(int j=0;j<=n;j++)
+            {
+                if(i<1||j<1)dp[i][j]=0;
+                if(i==1&&j==1)dp[i][j]=1;
+            }
+        }
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(i==1&&j==1)continue;
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        
+        return dp[m][n];
+}
+
+// { Driver Code Starts.
+
+ 
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n,m;
+		cin>>m>>n;
+	    cout << numberOfPaths(m, n)<<endl;
+	}
+    return 0;
+}  // } Driver Code Ends
