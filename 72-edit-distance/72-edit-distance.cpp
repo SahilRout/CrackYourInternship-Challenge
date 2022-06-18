6043a3d5e76d0ebf35e51e1ac86c8dc6bfe3dacb
SahilRout <85143180+SahilRout@users.noreply.github.com>
2022-06-18 12:33:20 +0530

    Time: 58 ms (22.35%), Space: 8.4 MB (67.46%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -15,7 +15,21 @@ public:
     int minDistance(string s1, string s2) {
         int m=s1.size();
         int n=s2.size();
-        memset(dp,-1,sizeof(dp));
-        return solve(s1,s2,m-1,n-1);
+        int dp[m+1][n+1];
+        for(int i=0;i<=n;i++)dp[0][i]=i;
+        for(int j=0;j<=m;j++)dp[j][0]=j;
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(s1[i-1]==s2[j-1])
+                {
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else dp[i][j]=1+min(dp[i][j-1],min(dp[i-1][j],dp[i-1][j-1]));
+            }
+        }
+        
+        return dp[m][n];
     }
 };
