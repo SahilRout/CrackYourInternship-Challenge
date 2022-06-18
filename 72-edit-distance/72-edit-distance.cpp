0be343ca64b47755c16f9c1fd2d713f5afc8e23e
SahilRout <85143180+SahilRout@users.noreply.github.com>
2022-06-18 12:42:08 +0530

    Time: 10 ms (86.01%), Space: 7.2 MB (84.36%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,17 +1,5 @@
 class Solution {
 public:
-    int dp[501][501];
-    int solve(string s1,string s2,int i,int j)
-    {
-        if(i<0)return j+1;
-        if(j<0)return i+1;
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(s1[i]==s2[j])
-        {
-            return dp[i][j]=solve(s1,s2,i-1,j-1);
-        }
-        return dp[i][j]=1+min(solve(s1,s2,i,j-1),min(solve(s1,s2,i-1,j),solve(s1,s2,i-1,j-1)));
-    }
     int minDistance(string s1, string s2) {
         int m=s1.size();
         int n=s2.size();
