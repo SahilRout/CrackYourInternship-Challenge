5757e933fa4e4782745908836914040f57de4b0b
SahilRout <85143180+SahilRout@users.noreply.github.com>
2022-06-22 16:53:07 +0530

    Time: 1660 ms (5.00%), Space: 37.5 MB (11.75%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -7,10 +7,9 @@ public:
         {
             return 0;
         }
-        if(prev==-1)return max(1+solve(nums,i+1,i),solve(nums,i+1,prev));
-        if(prev!=-1&&dp[i][prev]!=-1)return dp[i][prev];
-        if(nums[prev]>=nums[i])return dp[i][prev]=solve(nums,i+1,prev);
-        return dp[i][prev]=max(1+solve(nums,i+1,i),solve(nums,i+1,prev));
+        if(dp[i][prev+1]!=-1)return dp[i][prev+1];
+        if(prev==-1||nums[i]>nums[prev])return dp[i][prev+1]=max(1+solve(nums,i+1,i),solve(nums,i+1,prev));
+        return dp[i][prev+1]=solve(nums,i+1,prev);
     }
     int lengthOfLIS(vector<int>& nums) {
         memset(dp,-1,sizeof(dp));
