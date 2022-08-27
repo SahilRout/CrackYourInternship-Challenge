6a616ccdb478491888ed6f151899525751947856
SahilRout <85143180+SahilRout@users.noreply.github.com>
2022-08-27 11:06:42 +0530

    Time: 232 ms (5.59%), Space: 46.5 MB (8.13%) - LeetHub

---
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        map<int,int> mp;
+        vector<int> ans;
+        for(auto it : nums)
+        {
+            mp[it]++;
+        }
+        for(auto it : mp)
+        {
+            if(it.second==2)ans.push_back(it.first);
+        }
+        return ans;
+    }
+};
